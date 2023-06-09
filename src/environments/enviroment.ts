4036315c5e73ce90739c4d319a6ebafcce9a17b5
Lilimonchik <obertynskiya@gmail.com>
2023-06-09 23:28:38 +0300

    06.06.2023(connetion for back-end)

--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,6 +1,6 @@
 export const enviroment = {
   prodution: false,
-  authApi: 'https://localhost:7095/',
-  tokenWhiteListedDomains: ['localhost:7095'],
+  authApi: 'https://hetel-api.herokuapp.com/',
+  tokenWhiteListedDomains: ['hetel-api.herokuapp.com'],
   check: true,
 }
